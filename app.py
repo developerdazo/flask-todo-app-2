98343eb54574a97694c90cce2a640110ed8faf00
goodgame <commandblock1655@gmail.com>
2021-09-05 17:07:30 +0900

    no message

--- app.py
+++ app.py
@@ -1,13 +1,47 @@
-from flask import Flask, render_template
+from datetime import datetime
+
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30), nullable=False)
+    detail = db.Column(db.String(100))
+    due = db.Column(db.DateTime, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
 
 
-@app.route('/')
+
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    sum=['たこやき','おいしい','マヨネーズ','と','ソース','いつも','かけてる','まじで',"おいしい"]
-    return render_template("index.html",sum=sum)
+    if request.method == 'GET':
+        posts = Post.query.all()
+        return render_template('index.html', posts=posts)
 
+    else:
+        title = request.form.get('title')
+        detail = request.form.get('detail')
+        due = request.form.get('due')
+        name = request.form.get('name')
+
+        due = datetime.strptime(due, '%Y-%m-%d')
+        new_post = Post(title=title, detail=detail, due=due, name=name)
+
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/')
+
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
 
 
 
